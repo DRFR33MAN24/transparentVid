41e3b683b66b3e5c143901105c93c99d30c073a8
Gordon Freeman <drfr33man@protonmail.com>
2022-10-09 19:35:01 +0300

    implementing zooming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
+const zoomStep = 20;
 const App = () => {
-  useEffect(() => {}, []);
+  const ref = useRef();
+  const [videoZoom, setVideoZoom] = useState(200);
+
+  useEffect(() => {
+    function zoomIn() {
+      // dont name single functions in caps, use camelcase
+      setVideoZoom((current) => current + zoomStep);
+    }
+    function zoomOut() {
+      // dont name single functions in caps, use camelcase
+      setVideoZoom((current) => current - zoomStep);
+    }
+    function handleKeyDown(e) {
+      console.log(e.key);
+      if (e.key === "+") {
+        zoomIn();
+      }
+      if (e.key === "-") {
+        zoomOut();
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Don't forget to clean up
+    return function cleanup() {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
-    <div>
-      <video loop autoPlay={true}>
+    <div ref={ref}>
+      <video loop autoPlay={true} width={videoZoom}>
         <source src="vid.webm" type="video/webm" />
       </video>
     </div>
